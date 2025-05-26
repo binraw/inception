fe8d5adc18c571e80093724c1eb2c3bb7a53ef92
rtruvelo <rtruvelo@rtruvelo.rtruvelo.42.fr>
2025-05-26 17:06:55 +0200

    script mariadb

---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# dans son initialisation je vais devoir :
+# attendre que mariadb soit comletement demarre 
+
+# creer la base de donnees si elle existe pas
+
+# creee l'utilisateur et lui donner les droits
+
+# appliquer les changement ? flush privileges ? 
+ 
+until mysql -h "localhost" -u "root" -p"$MYSQL_ROOT_PASSWORD" -e "SHOW DATABASE;"; do 
+    echo "MariaDB ..."
+    sleep 3
+done 
+
+mysql -h "localhost" -u "root" -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXITS $MYSQL_DATABASE;"
+
+mysql -h "localhost" -u "root" -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER IF NOT EXITS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mysql -h "localhost" -u "root" -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+
+
+mysql -h "localhost" -u "root" -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+
+echo "utilisateur creer."
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+# je dis config le fichier wp-config.php 
+# renommer wp-config-sample.php
+#je dois modifier le wp-config.php pour definir mes parametres de ma base de donnee
+# je vais utiliser sed pour modifier ca
+#une fois que wp-config.php est configurer je peux installer wordpress avec
+#wp-cli
+#puis je creer des utilisateurs et je dois installer des plugins
+# ensuite je dois lancer php-fpm et nginx 
+# peut etre configurer php-fpm
+
+
+
 echo "listen = 0.0.0.0:9000" >> /etc/php83/php-fpm.d/www.conf 
